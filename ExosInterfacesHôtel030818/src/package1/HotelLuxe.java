e782782d8eb56fbfeb2634d82bf296f36f88c1d1
Dhont Laetitia <laetitiadhont75@gmail.com>
2018-08-03 15:48:24 +0200

    first commit upload all files

---
+++ ExosInterfacesHôtel030818/src/package1/HotelLuxe.java
@@ -0,0 +1,16 @@
+package package1;
+/*
+ * Projet Hotel Interface
+1-Quelles méthodes communes doivent avoir des HotelLuxe (par défaut) ?
+    exemple : méthode descriptionPiscineLuxe(); la description de cette piscine de luxe ne sera pas la même (7 étoiles plus luxueux que 5 étoiles), reserverTaxiLuxe() (pas le même type de taxi de luxe) ect
+    Créer une interface HotelLuxe
+
+2-Créer 2 classes qui respectent et implémenter l’interface HotelLuxe (ex : chalet, airbnb luxe, palais, chambreZoo …)
+    Créer une classe HotelCinqEtoiles et HotelSeptEtoiles qui implémentent et respectent l’interface HotelLuxe et redéfinisse la méthode descriptionPiscineLuxe() et reserverTaxiLuxe()
+
+3-Créer une classe application. Dans le main, créer des instances qui testent vos 2 classes (qui respecteront 2 interfaces)
+ */
+
+public interface HotelLuxe {
+
+}
